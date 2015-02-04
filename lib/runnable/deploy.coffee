forio/forio-cli

commit e9f1b94244db94b90a879c75293ee30759896104

make indentation consistent per linter

lib/runnable/deploy.coffee
@@ -34,9 +34,9 @@ uploadFile = (token, callback) ->
         callback()
 
 confirm = (str, onYes)->
-  process.stdout.write "#{str} (Y/n) "
-  process.stdin.setEncoding "utf8"
-  process.stdin.once "data", (val)->
+    process.stdout.write "#{str} (Y/n) "
+    process.stdin.setEncoding "utf8"
+    process.stdin.once "data", (val)->
     if val.trim() is "Y"
         process.stdin.resume()
         onYes()

lib/util/authenticate.coffee
@@ -10,8 +10,8 @@ authenicate = (userName, password, host, simPath, callback)->
         path: "/simulate/api/authentication/#{simPath}"
         method: 'POST'
         headers:
-          'Content-Type': 'application/x-www-form-urlencoded'
-          'Content-Length': auth_params.length
+            'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Length': auth_params.length
     if host[1]
         login_connection_param.port = host[1]
 

commit d41b2366e9cf87ca14dea70bf66ccb7f682aa3b8

correct indentation regression from e9f1b94244db94b90a879c75293ee30759896104

lib/runnable/deploy.coffee
@@ -38,11 +38,11 @@ confirm = (str, onYes)->
     process.stdout.write "#{str} (Y/n) "
     process.stdin.setEncoding "utf8"
     process.stdin.once "data", (val)->
-    if val.trim() is "Y"
-        process.stdin.resume()
-        onYes()
-    else
-        die()
+        if val.trim() is "Y"
+            process.stdin.resume()
+            onYes()
+        else
+            die()
 
 createTempZip = (local, tempFile, callback) ->
     exec "rm #{tempFile}", ()->

lib/runnable/deploye.coffee
@@ -38,11 +38,11 @@ confirm = (str, onYes)->
     process.stdout.write "#{str} (Y/n) "
     process.stdin.setEncoding "utf8"
     process.stdin.once "data", (val)->
-    if val.trim() is "Y"
-        process.stdin.resume()
-        onYes()
-    else
-        die()
+        if val.trim() is "Y"
+            process.stdin.resume()
+            onYes()
+        else
+            die()
 
 createTempZip = (local, tempFile, callback) ->
     exec "rm #{tempFile}", ()->